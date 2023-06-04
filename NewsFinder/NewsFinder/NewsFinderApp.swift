5af90de5ce7c6fe9ac80ca0c894034893151cea1
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-06-04 16:15:13 +0300

    Initial commit

---
+++ NewsFinder/NewsFinder/NewsFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFinderApp.swift
+//  NewsFinder
+//
+//  Created by Ahmed Shaban on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
