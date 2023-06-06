a5229c4773abe1d03fe1e2010374b2b7094fddd8
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-06-06 05:10:35 +0300

    added a navigation link to dummy article view

---
+++ NewsFinder/NewsFinder/Views/ArticleView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleView.swift
+//  NewsFinder
+//
+//  Created by Ahmed Shaban on 06/06/2023.
+//
+
+import SwiftUI
+
+struct ArticleView : View {
+    
+    
+    
+    var body: some View {
+        
+        Text("Hello Article")
+        
+    }
+    
+    
+}
--- NewsFinder/NewsFinder/Views/MainView.swift
+++ NewsFinder/NewsFinder/Views/MainView.swift
@@ -17,6 +17,7 @@ struct MainView: View {
     @State private var page = 1
 
     var body: some View {
+        NavigationView {
         VStack {
             ZStack {
                 Color.blue
@@ -27,67 +28,76 @@ struct MainView: View {
                     .foregroundColor(.white)
                     .padding()
             }
-
-            // MARK: - Filters
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
-                    ForEach(filters, id: \.self) { filter in
-                        Button(action: {
-                            self.filter = filter
-                        }) {
-                            Text(filter)
-                                .font(.footnote)
-                                .padding()
-                        }
-                        .foregroundColor(self.filter == filter ? Color.white : Color.blue)
-                        .background(self.filter == filter ? Color.blue : Color.white)
-                        .frame(height: 40)
-                        .cornerRadius(15)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 15)
-                                .stroke(Color.blue, lineWidth: 1)
-                        )
-                    }
-                }
-                .padding()
-            }
-            .frame(height: 50)
-            
-            // MARK: - List of Articles
-            List(articles, id: \.id) { article in
-                HStack() {
-                    // This is like AsyncImage() Introduced in iOS15, The Task requires iOS13 and above.
-                    RemoteImage(url: URL(string: article.urlToImage ?? "")) {
-                        ProgressView()
-                    }
-                    .frame(width: 100, height: 100)
-
-                    VStack(alignment: .leading) {
-                        Text(article.title)
-                            .font(.headline)
-                            .padding(.vertical, 4)
-                            .onAppear {
-                                if article.title == articles.last?.title {
-                                    scrolledToEnd = true
-                                    // Filter Debugger messages using "Hello"
-                                    print("Hello: number of articles is \(articles.count)")
-                                }
+                // MARK: - Filters
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(filters, id: \.self) { filter in
+                            Button(action: {
+                                self.filter = filter
+                            }) {
+                                Text(filter)
+                                    .font(.footnote)
+                                    .padding()
                             }
-
-                        Text("Author: \(article.author ?? "NA")")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
-
-                        Text("Source: \(article.source.name ?? "NA")")
-                            .font(.subheadline)
-                            .foregroundColor(.gray)
+                            .foregroundColor(self.filter == filter ? Color.white : Color.blue)
+                            .background(self.filter == filter ? Color.blue : Color.white)
+                            .frame(height: 40)
+                            .cornerRadius(15)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color.blue, lineWidth: 1)
+                            )
+                        }
                     }
+                    .padding()
                 }
-                .padding()
-                .background(Color.white)
-                .cornerRadius(15)
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+                .frame(height: 50)
+                
+                // MARK: - List of Articles
+                List(articles, id: \.id) { article in
+                    
+                    ZStack {
+                        NavigationLink(destination: ArticleView()) { EmptyView() }.opacity(0.0)
+                        
+                        HStack() {
+                            // This is like AsyncImage() Introduced in iOS15, The Task requires iOS13 and above.
+                            RemoteImage(url: URL(string: article.urlToImage ?? "")) {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
+                            
+                            VStack(alignment: .leading) {
+                                Text(article.title)
+                                    .font(.headline)
+                                    .padding(.vertical, 4)
+                                    .onAppear {
+                                        if article.title == articles.last?.title {
+                                            scrolledToEnd = true
+                                            // Filter Debugger messages using "Hello"
+                                            print("Hello: number of articles is \(articles.count)")
+                                        }
+                                    }
+                                
+                                Text("Author: \(article.author ?? "NA")")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                                
+                                Text("Source: \(article.source.name ?? "NA")")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .shadow(color: Color.black.opacity(0.1), radius: 4, x: 0, y: 2)
+                    }
+                        
+                    
+                }
+
             }
+            
         }
         .onAppear {
             loadArticles()
